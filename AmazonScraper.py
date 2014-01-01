ebe6e68b68cfc26eb3c11f5a0ea0b08124a73dc2
torresmateo <torresmateo@gmail.com>
2014-01-01 19:49:28 -0300

    moved scraper to scrapy

--- AmazonScraper.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-
-#this module reads content from a valid amazon wishlist url and navigates through the links
-#retrieve information about prices
-
-class AmazonScraper():
-	
