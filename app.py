9c34f71bff88ea9bad03bfc3c5644520a1bc427c
torresmateo <torresmateo@gmail.com>
2015-02-21 22:30:57 -0300

    added Flask (ultra basic)

--- app.py
+++ app.py
@@ -1,29 +1,22 @@
 __author__ = 'torresmateo'
 
-from twisted.internet import reactor
-from scrapy.crawler import Crawler
-from scrapy import log, signals
-from wishlistSentinel.spiders.amazonSpider import AmazonSpider
-from scrapy.utils.project import get_project_settings
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
-items = []
+# configuration
+DATABASE = '/tmp/database.db'
+DEBUG = True
+SECRET_KEY = 'dev key'
+USERNAME = 'admin'
+PASSWORD = 'default'
 
-def add_item(item):
-    items.append(item)
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
 
-def stop_reactor():
-    print "hola"
-    reactor.stop()
+# create application
+app = Flask(__name__)
+app.config.from_object(__name__)
 
-log.start()
-spider = AmazonSpider("http://www.amazon.com/gp/registry/wishlist/12XDZWAFB377I")
-settings = get_project_settings()
-crawler = Crawler(settings)
-crawler.signals.connect(stop_reactor, signal=signals.spider_closed)
-crawler.signals.connect(add_item, signal=signals.item_passed)
-crawler.configure()
-a = crawler.crawl(spider)
-crawler.start()
-reactor.run() # the script will block here until the spider_closed signal was sent
-
-
+#run app
+if __name__ == '__main__':
+    app.run()
