31c2d7e25d9551ebe93e4f1458e63cdc2798bf96
torresmateo <torresmateo@gmail.com>
2015-02-22 22:39:08 -0300

    little advance

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ __author__ = 'torresmateo'
 
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
 
 # configuration
 DATABASE = '/tmp/database.db'
@@ -13,6 +14,11 @@ PASSWORD = 'default'
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 # create application
 app = Flask(__name__)
 app.config.from_object(__name__)
